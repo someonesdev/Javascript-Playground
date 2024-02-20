0b6aa3ac576b87079862f49887e5e2257181030b
someonesdev <someonesdev@gmail.com>
2024-02-19 23:49:40 -0600

    Added "New task" functionality

--- To-do list/script.js
+++ To-do list/script.js
@@ -1,1 +1,14 @@
+
 "use strict";
+
+let taskList = document.querySelector("#task-list");
+
+let newTaskBtn = document.querySelector(".new-task");
+newTaskBtn.addEventListener("click", function () {
+    let task = String(document.querySelector("#task-input").value);
+    let newListItem = document.createElement("li");
+    let newSpan = document.createElement("span");
+    newSpan.textContent = task;
+    newListItem.appendChild(newSpan);
+    taskList.appendChild(newListItem);
+});
