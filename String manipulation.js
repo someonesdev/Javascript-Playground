710b6a79ae0b63689fd59f703d18eb982c2ee882
someonesdev <someonesdev@gmail.com>
2024-03-20 02:10:46 -0600

    Exercises

--- String manipulation.js
+++ String manipulation.js
@@ -5,10 +5,24 @@
 function fullName(fullname) {
   let name = fullname.toLowerCase().split(" ");
   let newName = [];
-  for (let i = 0; i < name.length; i++){
-    newName.push(name[i][0].toUpperCase() + name[i].slice(1));
+  for (let i = 0; i < name.length; i++) {
+    // newName.push(name[i][0].toUpperCase() + name[i].slice(1));
+    newName.push(name[i].replace(name[i][0], name[i][0].toUpperCase()));
   }
   return newName.join(" ");
 }
 
 fullName("jorge enrique zúñiga landeros");
+
+/* Create a function that padds a credit card number */
+
+function maskingCC(ccnumber) {
+  return ccnumber.slice(-4).padStart(14, "*");
+}
+
+maskingCC("1382-2894-2938-9847");
+
+// Repeat
+
+let message = "Nothing to worry about. ";
+let newMessage = message.repeat(5);
