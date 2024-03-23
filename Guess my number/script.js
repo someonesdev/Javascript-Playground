02d29452e4910e2f28b1d5cae9d80bccfae116e2
someonesdev <someonesdev@gmail.com>
2024-03-23 01:08:41 -0600

    Added scoring functionality

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -5,22 +5,34 @@ let secretNumber = document.querySelector(".number").textContent;
 let message = document.querySelector(".message");
 let guess = document.querySelector(".guess");
 let checkButton = document.querySelector(".check");
+let score = document.querySelector(".score");
+
+let gameScore = 20;
+let gameHighScore = 0;
 
 checkButton.addEventListener("click", check);
-guess.addEventListener('keydown', function(e){
-    if(e.key === 'Enter'){
-        check();
-    }
-})
+guess.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    check();
+  }
+});
 
 function check() {
   let guess = document.querySelector(".guess").value;
   console.log(guess);
-  if (guess == randomNumber) {
-    message.textContent = "YOU WON!";
-  } else if (!guess) {
+  if (!guess) {
     message.textContent = "That is not a valid number.";
+  } else if (guess == randomNumber) {
+    message.textContent = "YOU WON!";
+    checkButton.disabled = true;
+    guess.disabled = true;
+  } else if (guess > randomNumber) {
+    gameScore--;
+    score.textContent = gameScore;
+    message.textContent = "Go lower!";
   } else {
-    message.textContent = "Try again!";
+    gameScore--;
+    score.textContent = gameScore;
+    message.textContent = "Go higher!";
   }
 }
