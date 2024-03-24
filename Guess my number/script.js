8514da3446941455d0c9bcbba2350ceb5a52eee4
someonesdev <someonesdev@gmail.com>
2024-03-23 23:57:41 -0600

    Secret number functionality

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -1,11 +1,11 @@
 let randomNumber = Math.floor(Math.random() * 20) + 1;
-console.log(randomNumber);
 
 let secretNumber = document.querySelector(".number").textContent;
 let message = document.querySelector(".message");
 let guess = document.querySelector(".guess");
 let checkButton = document.querySelector(".check");
 let score = document.querySelector(".score");
+let number = document.querySelector(".number");
 
 let gameScore = 20;
 let gameHighScore = 0;
@@ -17,15 +17,20 @@ guess.addEventListener("keydown", function (e) {
   }
 });
 
+function winning() {
+  message.textContent = "YOU WON!";
+  checkButton.disabled = true;
+  guess.disabled = true;
+  number.textContent = randomNumber;
+}
+
 function check() {
   let guess = document.querySelector(".guess").value;
   console.log(guess);
   if (!guess) {
     message.textContent = "That is not a valid number.";
   } else if (guess == randomNumber) {
-    message.textContent = "YOU WON!";
-    checkButton.disabled = true;
-    guess.disabled = true;
+    winning();
   } else if (guess > randomNumber) {
     gameScore--;
     score.textContent = gameScore;
