9ba2151104ee882854807da78ff70dd29b197320
someonesdev <someonesdev@gmail.com>
2024-04-05 00:28:05 -0600

    Default parameters

--- The airlines.js
+++ The airlines.js
@@ -1,3 +1,17 @@
 "use strict";
 
-const priceGB = '288,97'
+// Setting default parameters
+
+let bookings = [];
+
+const createBooking = function(flightNum = 0, numPassengers = 0, price = '$000'){
+    const  bookingInfo = {
+        flightNum,
+        numPassengers, 
+        price
+    }
+    bookings.push(bookingInfo);
+    return bookings;
+};
+
+createBooking(243, undefined, 656);
