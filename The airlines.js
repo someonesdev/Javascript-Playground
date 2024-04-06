3c629ed3838f4948e13c95de511bad8458bae615
someonesdev <someonesdev@gmail.com>
2024-04-06 01:41:45 -0600

    Airlines

--- The airlines.js
+++ The airlines.js
@@ -4,14 +4,37 @@
 
 let bookings = [];
 
-const createBooking = function(flightNum = 0, numPassengers = 0, price = '$000'){
-    const  bookingInfo = {
-        flightNum,
-        numPassengers, 
-        price
-    }
-    bookings.push(bookingInfo);
-    return bookings;
+const createBooking = function (
+  flightNum = 0,
+  numPassengers = 0,
+  price = "$000"
+) {
+  const bookingInfo = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(bookingInfo);
+  return bookings;
 };
 
 createBooking(243, undefined, 656);
+
+const flight = "LH234";
+const passenger = {
+  name: "Jorge Zúñiga",
+  passport: 128372637,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 128372637) {
+    return "Checked in";
+  } else {
+    return "Wrong passport!";
+  }
+}
+
+checkIn(flight, passenger);
