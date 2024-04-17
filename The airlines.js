c311a8870d65760758be6135eb21cc8f611b99f0
someonesdev <someonesdev@gmail.com>
2024-04-16 23:32:15 -0600

    Updates

--- The airlines.js
+++ The airlines.js
@@ -4,15 +4,63 @@
 
 let bookings = [];
 
-const createBooking = function(flightNum = 0, numPassengers = 0, price = '$000'){
-    const  bookingInfo = {
-        flightNum,
-        numPassengers, 
-        price
-    }
-    bookings.push(bookingInfo);
-    return bookings;
+const createBooking = function (
+  flightNum = 0,
+  numPassengers = 0,
+  price = "$000"
+) {
+  const bookingInfo = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(bookingInfo);
+  return bookings;
 };
 
 createBooking(243, undefined, 656);
 
+const flight = "LH234";
+const passenger = {
+  name: "Jorge Zúñiga",
+  passport: 128372637,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 128372637) {
+    return "Checked in";
+  } else {
+    return "Wrong passport!";
+  }
+}
+
+checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+// Another example of using variables as parameters. When our parameters are objects, they will be affected.
+
+let newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+
+newPassport(passenger);
+checkIn(flight, passenger);
+
+// Call and apply methods.
+
+let lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    this.bookings.push({ flight: this.iataCode, name });
+    return `${name} booked a seat on ${this.airline} flight ${flightNum}`;
+  },
+};
+
+lufthansa.book(2172, "Jorge");
+lufthansa.bookings;
