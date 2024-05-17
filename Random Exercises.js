767a863fb213398cde849724b76e10407a903832
J. <109241853+someonesdev@users.noreply.github.com>
2024-05-17 15:21:50 +0000

    Array methods

--- Random Exercises.js
+++ Random Exercises.js
@@ -38,3 +38,15 @@ function capitalizing(anotherWord, firstLetter) {
 }
 
 capitalizing("miguel es un menso", firstLetter);
+
+// For each method
+
+let numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+for (let number of numbers){
+  console.log(number);
+}
