2d615c3c0fb30ed51eb3fa15bcbc479d95dad636
someonesdev <someonesdev@gmail.com>
2024-06-10 23:00:11 -0600

    Reduce

--- Reduce.js
+++ Reduce.js
@@ -4,29 +4,25 @@ let array = [1, 2, 3, 4, 5];
 
 console.log(array.reduce((number, total) => number + total, 0));
 
+Math.max(array);
+
 /* 
-Sum of Array Elements
-Product of Array Elements
 // Use reduce to calculate the product of array elements
-Maximum Value in Array
+
 // Use reduce to find the maximum value in the array
-Minimum Value in Array
+
 // Use reduce to find the minimum value in the array
-Counting Occurrences of Elements in Array
-let array = ['a', 'b', 'a', 'c', 'b', 'a'];
+
 // Use reduce to count the occurrences of each element in the array
-Flattening an Array of Arrays
-let array = [[1, 2], [3, 4], [5, 6]];
+let array = ['a', 'b', 'a', 'c', 'b', 'a'];
+
 // Use reduce to flatten the array of arrays
-Removing Duplicates from Array
-let array = [1, 2, 2, 3, 4, 4, 5];
+let array = [[1, 2], [3, 4], [5, 6]];
+
 // Use reduce to remove duplicates from the array
-Grouping Objects by Property
-let array = [{ age: 20 }, { age: 21 }, { age: 20 }];
+let array = [1, 2, 2, 3, 4, 4, 5];
+
 // Use reduce to group the objects by the 'age' property
-Array to Object Conversion
-let array = ['a', 'b', 'c'];
-// Use reduce to convert the array to an object where the keys are the
-Calculating Average of Array Elements
+let array = [{ age: 20 }, { age: 21 }, { age: 20 }];
 
 */
